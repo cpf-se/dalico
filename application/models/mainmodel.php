2511194c327d2efb71f5e9d30e0413186fb1d536
Christian Andersson <christian@cpf.se>
2011-05-10 17:34:03 +0200

    fix

--- application/models/mainmodel.php
+++ application/models/mainmodel.php
@@ -7,21 +7,26 @@ class MainModel extends CI_Model {
 	}
 
 	function get_all_patients($offset, $num_per_page) {
-		$this->db->select('patients.id as id, patients.token as token, lists.name as list, vcs.name as vcs');
-		$this->db->from('patients');
-		$this->db->join('lists', 'patients.list = lists.id', 'inner');
-		$this->db->join('vcs', 'lists.vc = vcs.id', 'inner');
-		$this->db->order_by('patients.id');
-		$this->db->limit($num_per_page, $offset);
-		$rs = $this->db->get();
-		$table = array();
-		if ($rs->num_rows() > 0) {
-			foreach ($rs->result_array() as $row) {
-				$table[$row['id']] = array('token' => $row['token'], 'list' => $row['list'], 'vcs' => $row['vcs']);
-			}
-		}
-		return array('total' => 11, 'pats' => $table);
-	}
+		$total = $this->db->get('patients')->num_rows();
 
+		if ($total > 0) {
+			$this->db->select('patients.id as id, patients.token as token, lists.name as list, vcs.name as vcs');
+			$this->db->from('patients');
+			$this->db->join('lists', 'patients.list = lists.id', 'inner');
+			$this->db->join('vcs', 'lists.vc = vcs.id', 'inner');
+			$this->db->order_by('patients.id');
+			$this->db->limit($num_per_page, $offset);
+			$rs = $this->db->get();
+			$table = array();
+			if ($rs->num_rows() > 0) {
+				foreach ($rs->result_array() as $row) {
+					$table[$row['id']] = array('token' => $row['token'], 'list' => $row['list'], 'vcs' => $row['vcs']);
+				}
+			}
+			return array('total' => $total, 'pats' => $table);
+		} else {
+			return array('total' => 0, 'pats' => array());
+		}
+	}
 }
 
