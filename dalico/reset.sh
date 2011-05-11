c20cdb8edc0cdc1bf1bc15a58717bc2cdd90004c
Christian Andersson <christian@cpf.se>
2011-05-11 20:02:32 +0200

    ported tank_auth schema to PostgreSQL (fixed)

--- dalico/reset.sh
+++ dalico/reset.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-psql -Atd dalico -c 'drop table crfedtrs'
-psql -Atd dalico -c 'drop table crfs'
-psql -Atd dalico -c 'drop table users'
-psql -Atd dalico -c 'drop table patients'
-psql -Atd dalico -c 'drop table lists'
-psql -Atd dalico -c 'drop table vcs'
-psql -Atd dalico -c 'drop table files'
+psql -Atd dalico -c 'drop table crfedtrs cascade'
+psql -Atd dalico -c 'drop table crfs cascade'
+psql -Atd dalico -c 'drop table users cascade'
+psql -Atd dalico -c 'drop table patients cascade'
+psql -Atd dalico -c 'drop table lists cascade'
+psql -Atd dalico -c 'drop table vcs cascade'
+psql -Atd dalico -c 'drop table files cascade'
+psql -Atd dalico -c 'drop table ci_sessions cascade'
+psql -Atd dalico -c 'drop table login_attempts cascade'
+psql -Atd dalico -c 'drop table user_autologin cascade'
+psql -Atd dalico -c 'drop table user_profiles cascade'
+psql -Atd dalico -c 'drop table dalby1 cascade'
 
