22578a8e5110b3ce71ed08252d59145d6111ef0e
Christian Andersson <christian@cpf.se>
2011-05-17 17:29:47 +0200

    main update to tank_auth

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -2,26 +2,32 @@
 
 class Main extends CI_Controller {
 
-	const ITEMS_PER_PAGE = 5;
+	const ITEMS_PER_PAGE = 10;
 
 	function __construct() {
 		parent::__construct();
-	}
 
-	function view($offset = 0) {
-		if (is_numeric($offset)) {
-			$offset = floor($offset);
-		} else {
-			$offset = 0;
-		}
-		$this->load->model('MainModel');
-		$data = $this->MainModel->get_all_patients($offset, Main::ITEMS_PER_PAGE);
-		$data['per_page'] = Main::ITEMS_PER_PAGE;
-		$this->load->view('main', $data);
+		$this->load->library('tank_auth');
 	}
 
 	function index($offset = 0) {
-		$this->view($offset);
+		if ($this->tank_auth->is_logged_in()) {
+			if (is_numeric($offset)) {
+				$offset = floor($offset);
+			} else {
+				$offset = 0;
+			}
+			$this->load->model('MainModel');
+			$data = $this->MainModel->get_all_patients($offset, Main::ITEMS_PER_PAGE);
+			$data['per_page'] = Main::ITEMS_PER_PAGE;
+			$this->load->view('main', $data);
+		} else {
+			redirect('/auth/login/');
+		}
 	}
+
+//	function index($offset = 0) {
+//		$this->view($offset);
+//	}
 }
 
